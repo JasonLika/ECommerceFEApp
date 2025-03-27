663fbf6efcbff6be8531e87c7394dfe944728f4c
Lika <2391440@cognizant.com>
2025-03-27 13:57:22 +0000

    Updated local url

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7117") });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://jasonlikaapiwebapp-hxf4amd6fsa4fab8.eastus2-01.azurewebsites.net/") });
 // Register the ProductService in the DI container
 builder.Services.AddScoped<ProductService>();
 builder.Services.AddScoped<TicketService>();
